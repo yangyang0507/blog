364d73d7630103e8d6fec6261884ad0e58e56eee
D.Yang <koyangslash@gmail.com>
2017-02-27 10:29:29 +0800

    update travis ci and theme next

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_a6b7c41c3c70_key -iv $encrypted_a6b7c41c3c70_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_a6b7c41c3c70_key -iv $encrypted_a6b7c41c3c70_iv -in .travis/travis.enc -out ~/.ssh/id_rsa -d
 # Set the permission of the key
 chmod 600 ~/.ssh/id_rsa
 # Start SSH agent
